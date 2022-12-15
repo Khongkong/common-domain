a9def88d4695ba0e13f2d91058cf377e818f9f2d
harbor.liu <harbor.liu@104.com.tw>
2022-12-15 18:35:13 +0800

    Bump php to 8.1 and add readonly on vo properties

--- src/ValueObjects/IntegerValue.php
+++ src/ValueObjects/IntegerValue.php
@@ -4,7 +4,7 @@ namespace KhongKong\Domain\Common\ValueObjects;
 
 abstract class IntegerValue extends ValueObject
 {
-    public function __construct(protected int $value)
+    public function __construct(protected readonly int $value)
     {
         $this->guard();
     }
--- src/ValueObjects/StringValue.php
+++ src/ValueObjects/StringValue.php
@@ -4,7 +4,7 @@ namespace KhongKong\Domain\Common\ValueObjects;
 
 abstract class StringValue extends ValueObject
 {
-    public function __construct(protected string $value)
+    public function __construct(protected readonly string $value)
     {
         $this->guard();
     }
